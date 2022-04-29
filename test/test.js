d3cbedef91b111c50fe96baaad800d0181e2f425
thedovekana <narcissekana90@gmail.com>
2022-04-29 11:13:09 +0000

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ describe('GET /', function() {
  it('respond with hello world', function(done) {
 
  //navigate to root and check the the response is "hello world"
-  request(app).get('/').expect('hello world', done);
+  request(app).get('/').expect('hey world', done);
    });
 
 });
