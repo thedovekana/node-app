e204f74ab9b643031f9fb998349073aeb849aaff
node-app-server <node1@example.com>
2022-04-29 11:22:30 +0000

    changement de hello world en hello guys

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 
  //Respond with "hello world" for requests that hit our root "/"
  app.get('/', function (req, res) {
-  res.send('hey world');
+  res.send('hello guys!');
   });
 
   //listen to port 3000 by default
